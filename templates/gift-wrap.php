b1d66b455fdacbcc5d192c2aca7acb7e34e9bc6c
pizzafritta <pizzafritta@gmail.com>
2016-03-08 10:37:48 +0100

    multi language translation shortcodes support
    Supports quickcodes [:en], [:it], [:ja] etc in "gift wrap message"

--- templates/gift-wrap.php
+++ templates/gift-wrap.php
@@ -1,3 +1,3 @@
 <p class="gift-wrapping" style="clear:both; padding-top: .5em;">
-	<label><?php echo str_replace( array( '{checkbox}', '{price}' ), array( $checkbox, $price_text ), wp_kses_post( $product_gift_wrap_message ) ); ?></label>
+	<label><?php echo _e(str_replace( array( '{checkbox}', '{price}' ), array( $checkbox, $price_text ), $product_gift_wrap_message )); ?></label>
 </p>
